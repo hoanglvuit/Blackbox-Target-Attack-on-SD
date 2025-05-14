baacdeabb386b4a5a5d062b35c447807149d2363
hoanglv <hoanglv282004@gmail.com>
2025-05-14 18:45:48 +0700

    fix find te

--- src/find_target_embedding.py
+++ src/find_target_embedding.py
@@ -20,7 +20,7 @@ def auto_tune_threshold(target_sentence,sentence,tokenizer,text_encoder,start = 
     ori_embedding = get_text_embeds_without_uncond(sentence, tokenizer=tokenizer, text_encoder=text_encoder)
     thres = np.arange(start,end,0.01) 
     for thre in thres : 
-        te = find_target_embedding_mask(target_embedding,ori_embedding,threshold=thre)
+        te = find_target_embedding_mask(target_embedding,ori_embedding,thres=thre)
         te_list = te.tolist()
         te = te.view(-1) 
         cosine = consine_similarity(target_embedding,ori_embedding,te) 
