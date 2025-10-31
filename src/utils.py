64bab8a3073835aac27e8759afd9e6e38a0e33ed
hoanglv <hoanglv282004@gmail.com>
2025-10-31 23:35:45 +0700

    debug

--- src/utils.py
+++ src/utils.py
@@ -11,6 +11,7 @@ def get_text_embeds_without_uncond(prompt, tokenizer, text_encoder, batch_size=2
         prompts = list(prompt)
 
     all_embeddings = []
+    print(f"Getting text embeddings for {len(prompts)} prompts")
     for start_idx in range(0, len(prompts), batch_size):
         batch_prompts = prompts[start_idx:start_idx + batch_size]
         text_input = tokenizer(
